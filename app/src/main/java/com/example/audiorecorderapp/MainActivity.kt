cf4f1d08023c0e03880f134a78e0687e421c7a7b
Emine INAN <emineinanx@gmail.com>
2023-03-07 16:25:36 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/audiorecorderapp/MainActivity.kt
+++ app/src/main/java/com/example/audiorecorderapp/MainActivity.kt
@@ -1,43 +1,87 @@
 package com.example.audiorecorderapp
 
+import android.Manifest
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults.buttonColors
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
+import com.example.audiorecorderapp.playback.AndroidAudioPlayer
+import com.example.audiorecorderapp.recorder.AndroidAudioRecorder
 import com.example.audiorecorderapp.ui.theme.AudioRecorderAppTheme
+import java.io.File
 
 class MainActivity : ComponentActivity() {
+    private val recorder by lazy { AndroidAudioRecorder(applicationContext) }
+    private val player by lazy { AndroidAudioPlayer(applicationContext) }
+    private var audioFile: File? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), 0)
+
         setContent {
             AudioRecorderAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+                    Button(
+                        onClick = {
+                            File(cacheDir, "audio.mp3").also {
+                                recorder.start(it)
+                                audioFile = it
+                            }
+                        },
+                        colors = buttonColors(backgroundColor = Red),
+                        modifier = Modifier.width(240.dp)
+                    ) {
+                        Text(text = "Start Recording", color = White, fontSize = 18.sp)
+                    }
+                    Spacer(modifier = Modifier.height(12.dp))
+                    Button(
+                        onClick = {
+                            recorder.stop()
+                        },
+                        colors = buttonColors(backgroundColor = Red),
+                        modifier = Modifier.width(240.dp)
+                    ) {
+                        Text(text = "Stop Recording", color = White, fontSize = 18.sp)
+                    }
+                    Spacer(modifier = Modifier.height(12.dp))
+                    Button(
+                        onClick = {
+                            player.playFile(audioFile ?: return@Button)
+                        },
+                        colors = buttonColors(backgroundColor = Red),
+                        modifier = Modifier.width(240.dp)
+                    ) {
+                        Text(text = "Play", color = White, fontSize = 18.sp)
+                    }
+                    Spacer(modifier = Modifier.height(12.dp))
+                    Button(
+                        onClick = {
+                            player.stop()
+                        },
+                        colors = buttonColors(backgroundColor = Red),
+                        modifier = Modifier.width(240.dp)
+                    ) {
+                        Text(text = "Stop Playing", color = White, fontSize = 18.sp)
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AudioRecorderAppTheme {
-        Greeting("Android")
     }
 }
